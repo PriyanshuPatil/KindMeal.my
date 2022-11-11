d6784d256a52ae0fecfeae3eab6b6da5cbd1946d
PriyanshuPatil <pantuugurjar@gmail.com>
2022-11-11 15:47:45 +0530

    added

--- huge-end-8625/src/App.js
+++ huge-end-8625/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer1 from './Components/Footer1';
 import Navbar from './Components/Navbar';
+import { Slider } from './Components/Slider';
+import { HomeData } from './Components/HomeData';
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <Slider/>
+     <HomeData/>
     </div>
   );
 }
